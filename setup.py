79038ab121646a60688567641646c3582738d2f5
Long Yang <ly2364@columbia.edu>
2020-03-18 12:43:20 -0400

    update authors

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ import setuptools
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="pkgname", # Replace with your own name
     version="0.0.1",
-    author="Example Author",
+    author="example author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
@@ -15,13 +16,14 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_dir={"pkgname": "pkgname"},
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
     ],
     entry_points={'console_scripts': [
             'pkgname = pkgname.main:main',],
         },
+    data_files = [("", ["LICENSE.txt"])]
     python_requires='>=3.6',
     zip_safe=False,
 )
